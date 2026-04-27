d00ecc202e39aa8f4d1e47f36e4a79ea1ad5e01e
Angel Serna <andres.hdzserna@gmail.com>
2026-04-27 15:43:46 -0600

    try & except

---
+++ instrucciones_try_&_except.py
@@ -0,0 +1,39 @@
+cadena1 = "123"
+entero = int(cadena1)
+#print(entero)
+
+cadena = input("Dame un numero positivo: ")
+'''
+El uso de try y except es para evitar que el 
+programa se detenga por un error, en este caso si el usuario 
+ingresa un valor que no se pueda convertir a entero, el programa no se detendrá, 
+sino que asignará un valor de -1 a la variable entero.
+'''
+
+try: 
+    entero = int(cadena)
+except:
+    entero = -1
+
+if entero > 0:
+    print("Valor correcto")
+else:
+    print("Valor incorrecto")
+
+#-------------------------------------------------------------
+#USOS AVANZADOOS DE IF
+lluvia = False
+print("Vamos a","la playa" if not lluvia else "la biblioteca")
+
+x = y = 40
+z = x + 1 if x > y else y + 2
+print(z)
+
+x = 3
+s = ('abc' if  (x == 1) else
+     'def' if (x == 2) else
+     'ghi' if (x == 3) else
+     'mno')
+print(s)
+
+print('Si' if ('jkl' in ['abc','def','ghi','jkl']) else 'No')
